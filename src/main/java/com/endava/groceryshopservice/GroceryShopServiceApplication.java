946ba99ef412fa7a4d53d919fdab6d04ca44469d
ifiodorova <myefij@gmail.com>
2021-10-25 16:13:13 +0300

    initial commit

---
+++ src/main/java/com/endava/groceryshopservice/GroceryShopServiceApplication.java
@@ -0,0 +1,14 @@
+package com.endava.groceryshopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryShopServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GroceryShopServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/endava/groceryshopservice/config/profiles/DbConnectionConfig.java
@@ -0,0 +1,22 @@
+package com.endava.groceryshopservice.config.profiles;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+@SuppressWarnings("unused")
+public class DbConnectionConfig {
+
+    @Profile("local")
+    @Bean
+    public void localDbConnection(){
+    }
+
+    @Profile("remote")
+    @Bean
+    public void remoteDbConnection(){
+    }
+}
