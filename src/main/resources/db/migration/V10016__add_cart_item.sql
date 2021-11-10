d09750defbb40049eb4c0062b246bc92371c0a41
Cristina Rotaru <Cristina.Rotaru@endava.com>
2021-11-10 10:30:50 +0200

    Add more items to cart

--- src/main/resources/db/migration/V10016__add_cart_item.sql
+++ src/main/resources/db/migration/V10016__add_cart_item.sql
@@ -1,13 +1,22 @@
-INSERT INTO t_items (
-                         id,
-                         product_id,
-                         quantity,
-                         cart_id
-)
-VALUES
-    (NEXT VALUE FOR grocery_shop_sequence,
-    (SELECT id FROM t_products WHERE name='Avocado'),
-    2,
-     (SELECT id FROM t_carts WHERE user_id = (SELECT user_id
-                                    FROM t_users
-                                    WHERE email = 'user@endava.com')));
+INSERT INTO t_items (id,
+                     product_id,
+                     quantity,
+                     cart_id)
+VALUES (NEXT VALUE FOR grocery_shop_sequence,
+             (SELECT id FROM t_products WHERE name = 'Avocado'),
+             2,
+             (SELECT id
+              FROM t_carts
+              WHERE user_id = (SELECT user_id
+                               FROM t_users
+                               WHERE email = 'user@endava.com'))),
+             (NEXT VALUE FOR grocery_shop_sequence,
+                     (SELECT id FROM t_products WHERE name = 'Garlic'),
+                  2,
+                 (SELECT id
+                  FROM t_carts
+                  WHERE user_id = (SELECT user_id
+                                   FROM t_users
+                                   WHERE email = 'user@endava.com'))
+
+);
