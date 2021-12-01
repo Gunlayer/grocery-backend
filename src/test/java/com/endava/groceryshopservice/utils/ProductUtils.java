ebdf6f81f2459c8e60cef15a10bad3e5aa89446e
Cristina Rotaru <Cristina.Rotaru@endava.com>
2021-12-02 00:02:53 +0200

    Added unit tests

--- src/test/java/com/endava/groceryshopservice/utils/ProductUtils.java
+++ src/test/java/com/endava/groceryshopservice/utils/ProductUtils.java
@@ -8,9 +8,11 @@ import lombok.NoArgsConstructor;
 
 import static com.endava.groceryshopservice.utils.TestConstants.DESCRIPTION;
 import static com.endava.groceryshopservice.utils.TestConstants.ID_ONE;
+import static com.endava.groceryshopservice.utils.TestConstants.ID_THREE;
 import static com.endava.groceryshopservice.utils.TestConstants.ID_TWO;
 import static com.endava.groceryshopservice.utils.TestConstants.IMAGE;
 import static com.endava.groceryshopservice.utils.TestConstants.NAME;
+import static com.endava.groceryshopservice.utils.TestConstants.NAME_TWO;
 import static com.endava.groceryshopservice.utils.TestConstants.PRICE;
 import static com.endava.groceryshopservice.utils.TestConstants.RATING;
 import static com.endava.groceryshopservice.utils.TestConstants.SIZES;
@@ -41,31 +43,9 @@ public class ProductUtils {
             .build();
 
     public static final Product PRODUCT_THREE = Product.builder()
-            .id(ID_TWO)
+            .id(ID_THREE)
             .image(IMAGE)
-            .name(NAME)
-            .price(PRICE)
-            .rating(RATING)
-            .description(DESCRIPTION)
-            .sizes(SIZES)
-            .sizeType(SizeTypes.KILOS)
-            .build();
-
-    public static final Product PRODUCT_FOUR = Product.builder()
-            .id(ID_TWO)
-            .image(IMAGE)
-            .name(NAME)
-            .price(PRICE)
-            .rating(RATING)
-            .description(DESCRIPTION)
-            .sizes(SIZES)
-            .sizeType(SizeTypes.KILOS)
-            .build();
-
-    public static final Product PRODUCT_FIVE = Product.builder()
-            .id(ID_TWO)
-            .image(IMAGE)
-            .name(NAME)
+            .name(NAME_TWO)
             .price(PRICE)
             .rating(RATING)
             .description(DESCRIPTION)
